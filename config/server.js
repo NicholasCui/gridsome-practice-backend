0c99d7f152474f0a212701b7606a8a45d311110a
Nicholas Cui <nicholas@bogon.tal-inc.com.cn>
2021-10-13 17:31:56 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd0500fae5a2c5e33675f90658ac911ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f98e5d03-f026-431e-a0a4-dc27d3e8fb27'
+};
